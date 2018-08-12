c50add32feb40036b49d9e604ca2dc41e0141e8a
AlexK <itvisa1c@gmail.com>
2018-08-12 09:53:19 +0000

    Lesson6

--- Lessons/Lesson6.py
+++ Lessons/Lesson6.py
@@ -1,6 +1,6 @@
 # patern
 # 
-
+# Fasad
 class Cat:
     say = 'meow'
 
@@ -36,13 +36,137 @@ class Flock():
         for i in range(self.count):
             self.set_ch.append(Chupakabra(name='Chu #{}'.format(i)))
     
-        
+    def kill_cat(self):
+        for i in self.set_ch:
+            if 'cat' in i.__dict__:
+                return i.kill()
+        return "Can't do it!"
+    
+class Store_Flock():
+    def __init__(self, flock):
+        self.set_ch = flock.set_ch
+        self.count = len(flock.set_ch)
+    
+    def make(self):
+        f = Flock()
+        f.set_ch = self.set_ch
+        f.count = self.count
+        return f
 
 # ch = Chupakabra()
 # print(ch.say, ch.say_like_cat, ch.kill(), ch.say_like_cat)
 f = Flock(count=5)
 f.make()
+f.set_ch[0].name = 'Strong'
 print(f.set_ch)
+
+sf = Store_Flock(f)
+del f
+#print(f.set_ch)
+f = sf.make()
+print(f.set_ch)
+for i in range(8):
+    print(f.kill_cat())
+
+# Store
+# class Cat:
+#     say = 'meow'
+
+# class Dog:
+#     say = 'gaw'
+
+# class Monter():
+#     def kill(self):
+#         del self.cat
+#         return 'Blood pf pf pf.'
+
+# class Chupakabra(Monter, Dog):
+#     def __init__(self, name=''):
+#         self.name = name
+#         self.cat = Cat()
+    
+#     @property
+#     def say_like_cat(self):
+#         try:
+#             return self.cat.say
+#         except AttributeError:
+#             return 'R.I.P'
+    
+#     def __repr__(self):
+#         return '<Chupakabra({})>'.format(self.name)
+
+# class Flock():
+#     def __init__(self, count=1):
+#         self.set_ch = []
+#         self.count = count
+
+#     def make(self):
+#         for i in range(self.count):
+#             self.set_ch.append(Chupakabra(name='Chu #{}'.format(i)))
+    
+# class Store_Flock():
+#     def __init__(self, flock):
+#         self.set_ch = flock.set_ch
+#         self.count = len(flock.set_ch)
+    
+#     def make(self):
+#         f = Flock()
+#         f.set_ch = self.set_ch
+#         f.count = self.count
+#         return f
+
+# # ch = Chupakabra()
+# # print(ch.say, ch.say_like_cat, ch.kill(), ch.say_like_cat)
+# f = Flock(count=5)
+# f.make()
+# f.set_ch[0].name = 'Strong'
+# print(f.set_ch)
+
+# sf = Store_Flock(f)
+# del f
+# #print(f.set_ch)
+# f = sf.make()
+# print(f.set_ch)
+    
+# sobiratel
+# class Cat:
+#     say = 'meow'
+
+# class Dog:
+#     say = 'gaw'
+
+# class Monter():
+#     def kill(self):
+#         del self.cat
+#         return 'Blood pf pf pf.'
+
+# class Chupakabra(Monter, Dog):
+#     def __init__(self, name=''):
+#         self.name = name
+#         self.cat = Cat()
+    
+#     @property
+#     def say_like_cat(self):
+#         try:
+#             return self.cat.say
+#         except AttributeError:
+#             return 'R.I.P'
+    
+#     def __repr__(self):
+#         return '<Chupakabra({})>'.format(self.name)
+
+# class Flock():
+#     def __init__(self, count=1):
+#         self.set_ch = []
+#         self.count = count
+
+#     def make(self):
+#         for i in range(self.count):
+#             self.set_ch.append(Chupakabra(name='Chu #{}'.format(i)))
+
+# f = Flock(count=5)
+# f.make()
+# print(f.set_ch)
 
 # abstraktnaya fabrica
 # class Cat:
